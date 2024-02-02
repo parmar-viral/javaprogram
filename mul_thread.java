922929959880835b5b2fed97a3bd58cf61237c5a
parmar-viral <parmarviral8324@gmail.com>
2024-02-02 19:16:18 +0530

    multiple threading in java

--- mul_thread.java
+++ mul_thread.java
@@ -1,1 +1,32 @@
-class a exten
+class a extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+            System.out.println("i="+i);
+        }
+    }
+}
+class b extends Thread{
+    public void run(){
+        for(int j=1;j<=10;j++){
+            System.out.println("j="+j);
+        }
+    }
+}
+class c extends Thread{
+    public void run(){
+        for(int k=1;k<=10;k++){
+            System.out.println("k="+k);
+        }
+    }
+}
+public class mul_thread {
+
+    public static void main(String[] args) {
+        a a1=new a();
+        b b1=new b();
+        c c1=new c();
+        a1.start();
+        b1.start();
+        c1.start();
+    }
+}
