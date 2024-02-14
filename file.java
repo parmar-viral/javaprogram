302081324b265276a97f1b882b3ba5ecf8f7fa26
parmar-viral <parmarviral8324@gmail.com>
2024-02-14 05:36:31 +0530

    RandomAccessFile in java

--- file.java
+++ file.java
@@ -9,14 +9,25 @@ public class file {
             }else{
                 System.out.println("File is already existing.............");
             } 
+            f.close();
          */
-        
-             // code for writing string into a file using FileWriter class
+        /*
+            // code for writing string into a file using FileWriter class
 
             FileWriter w=new FileWriter("f1.txt");
             w.write("java is a high level programming language............");
             System.out.println("successfully written into a file");
 
-         w.close();
+            w.close();
+         */
+
+        // code for RandomAccessFile .....................
+
+        RandomAccessFile RAF=new RandomAccessFile("f1.txt","rw"); 
+        RAF.writeUTF("RandomAccessFile Example in java");
+        System.out.println("successfully written into file using RandomAccessFile...."); 
+
+        RAF.close();
+            
     }
 }
