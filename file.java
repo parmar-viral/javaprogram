764c04dd0437932201849a505d5af1c8748e350f
parmar-viral <parmarviral8324@gmail.com>
2024-02-15 06:30:01 +0530

    update

--- file.java
+++ file.java
@@ -1,8 +1,11 @@
 import java.io.*;
 public class file {
     public static void main(String[] args) throws IOException{
+        
         /*
-            // code for creating a new file or check file is exixsting or not
+
+// code for creating a new file or check file is exixsting or not..................................
+
             File f=new File("f1.txt");
             if (f.createNewFile()) {
                 System.out.println("New file is created..........");
@@ -10,9 +13,10 @@ public class file {
                 System.out.println("File is already existing.............");
             } 
             f.close();
-         */
-        /*
-            // code for writing string into a file using FileWriter class
+         */        
+         /*
+            
+// code for writing string into a file using FileWriter class..........................................
 
             FileWriter w=new FileWriter("f1.txt");
             w.write("java is a high level programming language............");
@@ -21,19 +25,22 @@ public class file {
             w.close();
          */
 
-        // code for RandomAccessFile .....................
+// code for RandomAccessFile .....................................................................
 
-        RandomAccessFile RAF=new RandomAccessFile("RandomFile.txt","rw"); 
-       // RAF.write("RandomAccessFile Example".getBytes());
+       RandomAccessFile RAF=new RandomAccessFile("RandomFile.txt","rw"); 
+       
+      // RAF.write("RandomAccessFile Example".getBytes());
+
        RAF.write("hello".getBytes());
        RAF.write("\njava".getBytes());
        RAF.write("\nlanguage".getBytes());
 
       // RAF.seek(6);
       // RAF.write("JAVA".getBytes());
-        System.out.println("successfully written into file using RandomAccessFile...."); 
 
-        RAF.close();
+      System.out.println("successfully written into file using RandomAccessFile...."); 
+
+      RAF.close();
             
     }
 }
