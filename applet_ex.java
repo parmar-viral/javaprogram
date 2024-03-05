81afd24908be0e39f2ac40475284e556de75d492
parmar-viral <parmarviral8324@gmail.com>
2024-03-05 05:34:14 +0530

    update

--- applet_ex.java
+++ applet_ex.java
@@ -1,10 +1,12 @@
 import java.applet.*;
 import java.awt.*;
 public class applet_ex extends Applet {
-    public void paint(Graphics g){
+    public void paint(Graphics g, String sarif){
       
         Color c=new Color(100,0,100);
         g.setColor(c);
+       // Font f=new Font(sarif, 10, Font.BOLD);
+       // g.setFont(f);
         g.drawString("java applete example", 50, 50);
 
     }
